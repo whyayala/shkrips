a9819b73d1c17a0a184c5845eab9112138e858ee
Jacobi <jtayala89@gmail.com>
2025-10-15 12:47:29 -0500

    Update get-ca.sh

--- get-ca.sh
+++ get-ca.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# Verified on macos
-openssl s_client -showcerts -verify 5 -connect artifactory.forge-dev.rise8.us:443 < /dev/null
+# Verified on macos. Will write certificate to a file.
+echo | openssl s_client -showcerts -verify 5 -connect artifactory.forge-dev.rise8.us:443 -servername artifactory.forge-dev.rise8.us < /dev/null | openssl x509 > cert.pem

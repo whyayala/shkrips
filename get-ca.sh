6101671cfa78d9a63d464a28e04f4ea6aecf8554
Jacobi <jtayala89@gmail.com>
2025-10-15 12:50:23 -0500

    Update get-ca.sh
    parameterize

--- get-ca.sh
+++ get-ca.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# Verified on macos. Will write certificate to a file.
-echo | openssl s_client -showcerts -verify 5 -connect artifactory.forge-dev.rise8.us:443 -servername artifactory.forge-dev.rise8.us < /dev/null | openssl x509 > cert.pem
+# Verified on macos. Will write certificate for the passed in hostname to a file.
+echo | openssl s_client -showcerts -verify 5 -connect $1:443 -servername $1 < /dev/null | openssl x509 > cert.pem

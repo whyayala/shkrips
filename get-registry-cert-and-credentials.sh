790700c477361c8e058b225e7fb2fd6db40a2614
Jacobi <jtayala89@gmail.com>
2025-10-16 14:44:30 -0500

    Update get-registry-cert-and-credentials.sh
    add echoes for feedback

--- get-registry-cert-and-credentials.sh
+++ get-registry-cert-and-credentials.sh
@@ -4,16 +4,19 @@
 echo | openssl s_client -showcerts -verify 5 -connect $1:443 -servername $1 < /dev/null | openssl x509 > cert.pem
 
 # Podman by default does not run in rootful mode, so we will enable it temporarily.
+echo "--- Enabling rootful mode for podman to add cert ---"
 podman machine stop
 podman machine set --rootful
 podman machine start
 
 # Verified on macos. Will copy cert to podman and update it's trusts. May need to login to registry again or restart host.
+echo "--- Adding cert to podman machine for $1 ---"
 podman machine ssh podman-machine-default "mkdir -p /etc/containers/certs.d/$1" 
 podman machine cp ./cert.pem podman-machine-default:/etc/containers/certs.d/$1/ca.crt
 podman machine ssh podman-machine-default "update-ca-trust" 
 
 # Disable rootful mode
+echo "--- Disabling rootful mode for podman ---"
 podman machine stop
 podman machine set --rootful=false
 podman machine start
@@ -23,4 +26,5 @@ podman machine start
 # credentials in a different location than where docker compose looks for them. The simplest solution is to
 # just create a symlink from where podman login stores the cred to where docker compose will be looking for
 # them.
+echo "--- Creating symlink for compose to find podman auth credentials ---"
 ln -s ~/.config/containers/auth.json ~/.docker/config.json
